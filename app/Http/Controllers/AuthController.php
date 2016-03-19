6c9e2aa8c2f64eca080303aef78a016eb282260f
magamlag <lagovskiy@gmail.com>
2016-03-19 14:57:50 -0400

    Add validation to 'postSignUp' method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,22 @@ use App\Http\Requests;
 class AuthController extends Controller
 {
     //
-    public function getSignup() {
+    public function getSignUp() {
         return view( 'auth.signup' );
     }
 
-    public function postSignup( Request $request ){
-        
+    public function getSignIn() {
+        return view( 'auth.signin' );
+    }
+
+    public function postSignUp( Request $request ) {
+        $this->validate( $request, User::$rules );
+        User::create( [
+            'email'    => $request->input( 'email' ),
+            'username' => $request->input( 'username' ),
+            'password' => bcrypt( $request->input( 'password' ) ),
+        ] );
+
+        return redirect()->route( 'home' )->with( 'info', 'Your account has been created and you can now sign in' );
     }
 }
