50f5e32ec612e8f9d5d90fc9ce6607725ddef869
magamlag <lagovskiy@gmail.com>
2016-03-20 17:44:04 -0400

    Add links to 'Sign up' and 'Sing in' pages

--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -25,8 +25,8 @@
 {{--                                <li><a href="{{ route('profile.edit') }}">Update profile</a></li>--}}
 {{--                                <li><a href="{{ route('auth.signout') }}">Sign out</a></li>--}}
                                 {{--@else--}}
-{{--                                <li><a href="{{ route('auth.signup') }}">Sign up</a></li>--}}
-{{--                                <li><a href="{{ route('auth.signin') }}">Sign in</a></li>--}}
+                                <li><a href="{{ route('auth.signup') }}">Sign up</a></li>
+                                <li><a href="{{ route('auth.signin') }}">Sign in</a></li>
                                 {{--@endif--}}
                         </ul>
                 </div>
