7fdeae724df56d545e0f45b1f9b140de624f2a47
magamlag <lagovskiy@gmail.com>
2016-03-22 08:05:55 -0400

    Create getName and getNameOrUsername methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,22 @@ class User extends Authenticatable
         'username' => 'required|unique:users|alpha_dash|max:20',
         'password' => 'required|min:6',
     ];
+
+    public function getName( ){
+        if($this->first_name && $this->last_name)
+            return "{$this->first_name} {$this->last_name}";
+
+        if($this->first_name)
+            return "{$this->first_name}";
+
+        return null;
+    }
+
+    public function getNameOrUsername( ){
+        return $this->getName() ?: $this->username;
+    }
+
+    public function getFirstNameOrUsername(){
+        return $this->first_name ?: $this->username;
+    }
 }
