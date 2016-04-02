573e1e2dbb4eb6248fa5180a8ab373feb498a1bc
magamlag <lagovskiy@gmail.com>
2016-04-01 21:57:49 -0400

    button 'Add Friend'

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,17 +7,30 @@
           <hr>
      </div>
      <div class="col-lg-4 col-lg-offset-3">
-           @if(Auth::user()->hasFriendRequestPending($user))
-                <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
-           @endif
-          <h4>{{ $user->getFirstNameOrUsername() }}'s friends</h4>
-          @if(!$user->friends()->count())
-            <p>{{ $user->getFirstNameOrUsername() }} has no friends</p>
-          @else
-            @foreach($user->friends() as $user)
-              @include('user/partials/userblock')
-            @endforeach
-          @endif
+            @if (Auth::user()->hasFriendRequestPending($user))
+                 <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
+             @elseif (Auth::user()->hasFriendRequestReceived($user))
+                 <a href="#" class="btn btn-primary">Accept Friend Request</a>
+             @elseif (Auth::user()->isFriendsWith($user))
+                 <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
+
+                 <form action="#" method="POST">
+                     <input type="submit" value="Delete Friend" class="btn btn-primary">
+                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                 </form>
+            @else
+                 <a href="#" class="btn btn-primary">Add as friend</a>
+            @endif
+
+            <h4>{{ $user->getFirstNameOrUsername() }}'s friends</h4>
+
+            @if (!$user->friends()->count())
+                  <p>{{ $user->getFirstNameOrUsername() }} has no friends</p>
+            @else
+                    @foreach($user->friends() as $user)
+                        @include('user/partials/userblock')
+                 @endforeach
+            @endif
      </div>
   </div>
 @stop
