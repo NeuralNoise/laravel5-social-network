eecfb3f1b2c7ef858a13a2f752d3dcaf0dd050ec
magamlag <lagovskiy@gmail.com>
2016-04-12 22:19:28 -0400

    Show timeline page to user if he authorized

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,9 @@ class HomeController extends Controller
 {
     //
     public function index( ){
+        if ( Auth::check() ) {
+            return view( 'timeline.index' );
+        }
         return view( 'home' );
     }
 }
