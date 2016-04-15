d5702d9bc9067f1354beac947cfd1933f1e7e2f9
magamlag <lagovskiy@gmail.com>
2016-04-15 09:06:51 -0400

    Save data to 'body' in 'statuses' table

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -8,9 +8,14 @@ use App\Http\Requests;
 class StatusController extends Controller
 {
     //
-    public function postStatus(Request $request ){
-        $this->validate( $request, [
+    public function postStatus(Request $request)
+    {
+        $this->validate($request, [
             'status' => 'required|max:1000'
-        ] );
+        ]);
+
+        Auth::user()->statuses()->create(['body'=>$request->input('status')]);
+
+        return redirect()->route('home')->with('info', 'Status posted');
     }
 }
