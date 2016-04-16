95ada552cafb6389172b8571a65008a3bb4694e9
magamlag <lagovskiy@gmail.com>
2016-04-16 17:18:08 -0400

    Get statuses between users and direct them on timeline page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Models\Status;
 use App\Http\Controllers;
 use App\Http\Requests;
 
@@ -13,8 +14,8 @@ class HomeController extends Controller
         if ( Auth::check() ) {
             $statuses = Status::where(function($query){
                 $query->where('user_id', Auth::user()->id)
-                      ->orWhereIn('user_id', Auth::user()->friends->lists('id'));
-            });
+                      ->orWhereIn('user_id', Auth::user()->friends()->lists('id'));
+            })->orderBy('created_at', 'desc')->paginate(10);
             return view( 'timeline.index', compact('statuses') );
         }
         return view( 'home' );
