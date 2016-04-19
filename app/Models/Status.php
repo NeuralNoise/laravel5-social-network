aa5d05ac9e9cfcdccff0516bd793d97a3f9cdec2
magamlag <lagovskiy@gmail.com>
2016-04-18 20:02:16 -0400

    Build relation 'replies' based on the same model 'Status' and 'parent_id'

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,18 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Status extends Model {
-	//
-	protected $table = 'statuses';
-	protected $fillable = [
-			'body'
-	];
+class Status extends Model
+{
+    //
+    protected $table = 'statuses';
+    protected $fillable = [
+        'body'
+    ];
 
-    public function user( ){
-		return $this->belongsTo('App\Models\User', 'user_id');
-   }
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
 
-	public function scopeNotReply($query){
-		return $query->whereNull('parent_id');
-	}
+    public function scopeNotReply($query)
+    {
+        return $query->whereNull('parent_id');
+    }
+
+    public function replies()
+    {
+		return $this->hasMany('App\Models\Status', 'parent_id');
+    }
 }
