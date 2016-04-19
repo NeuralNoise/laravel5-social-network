afd656a83331208aeb0c00b87e8515d480cd48bf
magamlag <lagovskiy@gmail.com>
2016-04-18 20:03:31 -0400

    Add loop which shows all replies below parent status message

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -41,6 +41,25 @@
     					            <li>2 likes</li>
     					        </ul>
 
+    					 		@foreach ($status->replies as $reply)
+    						        <div class="media">
+    						            <a class="pull-left" href="{{ route('profile.index', ['username' => $reply->user->username]) }}">
+    						                <img class="media-object" alt="{{ $reply->user->getNameOrUsername() }}" src="{{ $reply->user->getAvatarUrl() }}">
+    						            </a>
+    						            <div class="media-body">
+    						                <h5 class="media-heading"><a href="{{ route('profile.index', ['username' => $reply->user->username]) }}">{{ $reply->user->getNameOrUsername() }}</a></h5>
+    						                <p>{{ $reply->body }}</p>
+    						                <ul class="list-inline">
+    						                    <li>{{ $reply->created_at->diffForHumans() }}.</li>
+    						                    @if ($reply->user->id !== Auth::user()->id)
+    						                    <li><a href="{{ route('status.like', ['statusId' => $reply->id]) }}">Like</a></li>
+    					                        @endif
+    					                        <li>{{ $reply->likes->count() }} {{ str_plural('like', $reply->likes->count()) }}</li>
+    						                </ul>
+    						            </div>
+    						        </div>
+    					        @endforeach
+    					        
     					        <form role="form" action="{{ route('status.reply', ['statusId' =>$status->id]) }}" method="post">
     					            <div class="form-group{{ $errors->has("reply-{$status->id}") ? ' has-error' : '' }}">
     					                <textarea name="reply-{{ $status->id }}" class="form-control" rows="2" placeholder="Reply to this status"></textarea>
