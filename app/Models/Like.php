d75d1c0f67b43e0cb6a59cb9e628f086e801c209
magamlag <lagovskiy@gmail.com>
2016-04-22 19:30:14 -0400

    Build morph relation

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     //
+    protected $table = 'likes';
+
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
 }
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -59,17 +59,18 @@
     						            </div>
     						        </div>
     					        @endforeach
-    					        
-    					        <form role="form" action="{{ route('status.reply', ['statusId' =>$status->id]) }}" method="post">
-    					            <div class="form-group{{ $errors->has("reply-{$status->id}") ? ' has-error' : '' }}">
-    					                <textarea name="reply-{{ $status->id }}" class="form-control" rows="2" placeholder="Reply to this status"></textarea>
-    					                @if ($errors->has("reply-{$status->id}"))
-    					                	<span class="help-block">{{ $errors->first("reply-{$status->id}") }}</span>
-    					                @endif
-    					            </div>
-    					            <input type="submit" value="Reply" class="btn btn-default btn-sm">
-    					            <input type="hidden" name="_token" value="{{ Session::token() }}">
-    					        </form>
+
+									<form role="form" action="{{ route('status.reply', ['statusId' =>$status->id]) }}" method="post">
+										<div class="form-group{{ $errors->has("reply-{$status->id}") ? ' has-error' : '' }}">
+											<textarea name="reply-{{ $status->id }}" class="form-control" rows="2" placeholder="Reply to this status"></textarea>
+											@if ($errors->has("reply-{$status->id}"))
+												<span class="help-block">{{ $errors->first("reply-{$status->id}") }}</span>
+											@endif
+										</div>
+										<input type="submit" value="Reply" class="btn btn-default btn-sm">
+										<input type="hidden" name="_token" value="{{ Session::token() }}">
+									</form>
+
     					    </div>
     					</div>
     					<!--FOR PAGINATION-->
