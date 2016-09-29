6a70a16a8073fb31b5d28575bf83366d0cfdf53b
magamlag <lagovskiy@gmail.com>
2016-09-28 21:39:14 -0400

    Add two methods: redirect and callback

--- app/Http/Controllers/FacebookAuthController.php
+++ app/Http/Controllers/FacebookAuthController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Socialite;
 
 class FacebookAuthController extends Controller
 {
     //
+    public function redirect(){
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callback(){
+        return Socialite::driver('facebook')->redirect();
+    }
 }
