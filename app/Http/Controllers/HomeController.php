ca2e232bb26eae893d741a952402f15c71c831c2
magamlag <lagovskiy@gmail.com>
2016-10-18 07:14:11 -0400

    Take out 'timeline' to a separate method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,14 +10,18 @@ use App\Http\Requests;
 class HomeController extends Controller
 {
     //
-    public function index( ){
-        if ( Auth::check() ) {
-            $statuses = Status::notReply()->where(function($query){
-                $query->where('user_id', Auth::user()->id)
-                      ->orWhereIn('user_id', Auth::user()->friends()->lists('id'));
-            })->orderBy('created_at', 'desc')->paginate(10);
-            return view( 'timeline.index', compact('statuses') );
-        }
-        return view( 'home' );
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function showCabinet()
+    {
+        $statuses = Status::notReply()->where(function ($query) {
+            $query->where('user_id', Auth::user()->id)
+                ->orWhereIn('user_id', Auth::user()->friends()->lists('id'));
+        })->orderBy('created_at', 'desc')->paginate(10);
+
+        return view('timeline.index', compact('statuses'));
     }
 }
