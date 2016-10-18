9e895a5f3a8dabb4c11de5211188c8ebefad6cfd
magamlag <lagovskiy@gmail.com>
2016-10-18 07:19:13 -0400

    Appoint middleware - 'guest' to 'SignIn' and 'SignUp' routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,34 +21,45 @@
 | kernel and includes session state, CSRF protection, and more.
 |
 */
+Route::get( '/', [ 'uses' => 'HomeController@index', 'as'   => 'home'	]);
+
+//Facebook
+Route::get('/redirect','FacebookAuthController@redirect');
+Route::get('/callback','FacebookAuthController@callback');
 
 Route::group( [ 'middleware' => [ 'web' ] ], function () {
-	//
-	Route::get( '/', [ 'uses' => 'HomeController@index', 'as'   => 'home'	]);
 
-	Route::get( 'signup', [ 'uses' => 'AuthController@getSignUp', 'as' => 'auth.signup' ] );
+	Route::group( [ 'middleware' => [ 'guest' ] ], function () {
+
+		Route::get( 'signin', [ 'uses' => 'AuthController@getSignIn', 'as' => 'auth.signin' ] );
+		Route::get( 'signup', [ 'uses' => 'AuthController@getSignUp', 'as' => 'auth.signup' ] );
+
+	});
+
 	Route::post( 'signup', [ 'uses' => 'AuthController@postSignUp', 'as' => 'auth.signup' ] );
 
-	Route::get( 'signin', [ 'uses' => 'AuthController@getSignIn', 'as' => 'auth.signin' ] );
 	Route::post( 'signin', [ 'uses' => 'AuthController@postSignIn', 'as' => 'auth.signin' ] );
 
 	Route::get( 'signout', [ 'uses' => 'AuthController@getSignOut', 'as' => 'auth.signout' ] );
 
-	Route::get( 'user/{username}', [ 'uses' => 'ProfileController@getProfile', 'as' => 'profile.index' ] );
+	Route::group( [ 'middleware' => [ 'auth' ] ], function () {
 
-	Route::get( 'profile/edit', [ 'uses' => 'ProfileController@getEdit', 'as' => 'profile.edit' ] );
-	Route::post( 'profile/edit', [ 'uses' => 'ProfileController@postEdit', 'as' => 'profile.edit' ] );
+		Route::get( 'cabinet', ['uses' => 'HomeController@showCabinet']);
 
-	Route::get( 'search', [ 'uses' => 'SearchController@getResults', 'as' => 'search.results' ] );
+		Route::get('user/{username}', ['uses' => 'ProfileController@getProfile', 'as' => 'profile.index']);
 
-	Route::get( 'friends', [ 'uses' => 'FriendController@getIndex', 'as' => 'friend.index' ] );
-	Route::get( 'friends/add/{username}', [ 'uses' => 'FriendController@getAdd', 'as' => 'friends.add' ] );
-	Route::get( 'friends/accept/{username}', [ 'uses' => 'FriendController@getAccept', 'as' => 'friends.accept' ] );
-	Route::post('status', [ 'uses' => 'StatusController@postStatus', 'as' => 'status.post' ]);
-	Route::post('status/{statusId}/reply', [ 'uses' => 'StatusController@postReply', 'as' => 'status.reply' ]);
-	Route::get('/status/{statusId}/like', [ 'uses'	=> 'StatusController@getLike', 'as'	=> 'status.like' ]);
+		Route::get('profile/edit', ['uses' => 'ProfileController@getEdit', 'as' => 'profile.edit']);
+		Route::post('profile/edit', ['uses' => 'ProfileController@postEdit', 'as' => 'profile.edit']);
 
-	//Facebook
-	Route::get('/redirect','FacebookAuthController@redirect');
-	Route::get('/callback','FacebookAuthController@callback');
+		Route::get('search', ['uses' => 'SearchController@getResults', 'as' => 'search.results']);
+
+		Route::get('friends', ['uses' => 'FriendController@getIndex', 'as' => 'friend.index']);
+		Route::get('friends/add/{username}', ['uses' => 'FriendController@getAdd', 'as' => 'friends.add']);
+		Route::get('friends/accept/{username}', ['uses' => 'FriendController@getAccept', 'as' => 'friends.accept']);
+		Route::post('status', ['uses' => 'StatusController@postStatus', 'as' => 'status.post']);
+		Route::post('status/{statusId}/reply', ['uses' => 'StatusController@postReply', 'as' => 'status.reply']);
+		Route::get('/status/{statusId}/like', ['uses' => 'StatusController@getLike', 'as' => 'status.like']);
+
+	});
+
 } );
