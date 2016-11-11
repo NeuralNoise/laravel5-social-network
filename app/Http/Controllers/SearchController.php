54f81464293a1a733d1f5e0ceb25423e90d07eef
magamlag <lagovskiy@gmail.com>
2016-11-11 17:37:35 -0500

    Add comments for method in SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,18 +9,24 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-  //
-  public function getResults(Request $request){
+  /**
+   * Get search key and return results
+   *
+   * @param Request $request
+   * @return \Illuminate\Contracts\View\Factory |\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+   */
+  public function getResults(Request $request)
+  {
 
-    $query = $request->input( 'query' );
+    $query = $request->input('query');
 
-    if ( !$query ) {
-      return redirect()->route( 'home' );
+    if (!$query) {
+      return redirect()->route('home');
     }
 
-    $users = User::where( DB::raw( "CONCAT(first_name, ' ' ,last_name)" ), 'LIKE', "%{$query}%" )
-                    ->orWhere('username', 'LIKE', "%{$query}%")
-                    ->get();
-    return view( 'search.results', compact('users') );
+    $users = User::where(DB::raw("CONCAT(first_name, ' ' ,last_name)"), 'LIKE', "%{$query}%")
+        ->orWhere('username', 'LIKE', "%{$query}%")
+        ->get();
+    return view('search.results', compact('users'));
   }
 }
