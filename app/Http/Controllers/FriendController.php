9042b10ae703422386ffa193e61d387d5e00236f
andrew <lagovskiy@gmail.com>
2017-04-13 11:21:59 -0400

    Formatting code to correspond PSR-1/PSR-2

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -15,12 +15,13 @@ class FriendController extends Controller
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function getIndex( ){
+    public function getIndex()
+    {
         $auth_user = Auth::user();
         $friends = $auth_user->friends();
         $friend_requests = $auth_user->friendRequests();
 
-        return view( 'friends.index', compact('friends', 'friend_requests') );
+        return view('friends.index', compact('friends', 'friend_requests'));
     }
 
     /**
@@ -29,33 +30,34 @@ class FriendController extends Controller
      * @param $username
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function getAdd( $username ) {
-        $user             = User::where( 'username', $username )->first();
-        $auth_user        = Auth::user();
+    public function getAdd($username)
+    {
+        $user = User::where('username', $username)->first();
+        $auth_user = Auth::user();
         $redirect_profile = redirect()
-            ->route( 'profile.index', [ 'username' => $user->username ] );
+            ->route('profile.index', ['username' => $user->username]);
 
-        if ( !$user ) {
-            return redirect()->back()->with( 'info', "User couldn't be find" );
+        if (!$user) {
+            return redirect()->back()->with('info', "User couldn't be find");
         }
 
-        if( Auth::user()->id === $user->id ){
-            return redirect()->route( 'home' );
+        if (Auth::user()->id === $user->id) {
+            return redirect()->route('home');
         }
 
-        if ( $auth_user->hasFriendRequestPending( $user ) ||
-            $user->hasFriendRequestPending( $auth_user )
+        if ($auth_user->hasFriendRequestPending($user) ||
+            $user->hasFriendRequestPending($auth_user)
         ) {
-            return $redirect_profile->with( 'info', 'Friend request already pending' );
+            return $redirect_profile->with('info', 'Friend request already pending');
         }
 
-        if ( $auth_user->isFriendsWith( $user ) ) {
-            return $redirect_profile->with( 'info', 'You are already friends' );
+        if ($auth_user->isFriendsWith($user)) {
+            return $redirect_profile->with('info', 'You are already friends');
         }
 
-        $auth_user->addFriend( $user );
+        $auth_user->addFriend($user);
 
-        return $redirect_profile->with( 'info', 'Friend request sent' );
+        return $redirect_profile->with('info', 'Friend request sent');
 
     }
 
@@ -65,20 +67,21 @@ class FriendController extends Controller
      * @param $username
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function getAccept( $username ){
-        $user = User::where( 'username', $username )->first();
+    public function getAccept($username)
+    {
+        $user = User::where('username', $username)->first();
 
-        if ( !$user ) {
-            return redirect()->back()->with( 'info', "User couldn't be find" );
+        if (!$user) {
+            return redirect()->back()->with('info', "User couldn't be find");
         }
 
-        if ( !Auth::user()->hasFriendRequestReceived($user) ) {
-            return redirect()->route( 'home' );
+        if (!Auth::user()->hasFriendRequestReceived($user)) {
+            return redirect()->route('home');
         }
 
         Auth::user()->acceptFriendRequest($user);
 
-        return redirect()->route( 'profile.index', [ 'username' => $username ] )
-                         ->with('info','Friend request accepted.');
+        return redirect()->route('profile.index', ['username' => $username])
+            ->with('info', 'Friend request accepted.');
     }
 }
