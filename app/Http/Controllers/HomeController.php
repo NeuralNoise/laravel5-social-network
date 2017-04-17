d09c653f4dac76e0c22a31e1a927a8fb3ae13709
andrew <lagovskiy@gmail.com>
2017-04-17 15:02:14 -0400

    Replace list method on pluck. In 5.3 this method deprecated.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     {
         $statuses = Status::notReply()->where(function ($query) {
             $query->where('user_id', Auth::user()->id)
-                ->orWhereIn('user_id', Auth::user()->friends()->lists('id'));
+                ->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
         })->orderBy('created_at', 'desc')->paginate(10);
 
         return view('timeline.index', compact('statuses'));
