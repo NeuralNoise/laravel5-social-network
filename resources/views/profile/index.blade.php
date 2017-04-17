26db589dcc3beeddc4dedd63f8086a8672579d8c
andrew <lagovskiy@gmail.com>
2017-04-17 15:59:16 -0400

    Rename route 'friend' to 'friends' in all places

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -72,16 +72,16 @@
             @if (Auth::user()->hasFriendRequestPending($user))
                 <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
             @elseif (Auth::user()->hasFriendRequestReceived($user))
-                <a href="{{ route('friend.accept', ['username' => $user->username]) }}" class="btn btn-primary">Accept Friend Request</a>
+                <a href="{{ route('friends.accept', ['username' => $user->username]) }}" class="btn btn-primary">Accept Friend Request</a>
             @elseif (Auth::user()->isFriendsWith($user))
                 <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
 
-                <form action="{{ route('friend.delete', ['username' => $user->username]) }}" method="POST">
+                <form action="{{ route('friends.delete', ['username' => $user->username]) }}" method="POST">
                     <input type="submit" value="Delete Friend" class="btn btn-primary">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 </form>
             @elseif (Auth::user()->id !== $user->id)
-                <a href="{{ route('friend.add', ['username' => $user->username]) }}" class="btn btn-primary">Add as friend</a>    
+                <a href="{{ route('friends.add', ['username' => $user->username]) }}" class="btn btn-primary">Add as friend</a>
             @endif
 
             <h4>{{ $user->getFirstNameOrUsername() }}'s friends</h4>
