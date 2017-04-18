fceab635895e1e50a722a22b6f195b2f81ec7faf
andrew <lagovskiy@gmail.com>
2017-04-18 17:33:47 -0400

    Add tests to 'signup' functionality

--- tests/Browser/SignUpTest.php
+++ tests/Browser/SignUpTest.php
@@ -8,16 +8,39 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class SignUpTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function test_signup_user()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('SocialNet');
+            $browser->visit('/signup')
+                ->type('email', 'test1@gustr.com')
+                ->type('username', 'test1')
+                ->type('password', 'test')
+                ->assertSee('SocialNet');
+        });
+    }
+
+    /** @test */
+    public function test_email_field_require()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/signup')
+                ->type('username', 'test1')
+                ->type('password', 'test')
+                ->press('.submit')
+                ->assertSee('The email field is required');
+        });
+    }
+
+    /** @test */
+    public function it_returns_all_validation_errors()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('/signup')
+                ->press('.submit')
+                ->assertSee('The email is required')
+                ->assertSee('The username  is required')
+                ->assertSee('The password is required');
         });
     }
 }
