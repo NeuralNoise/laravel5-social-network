cb165d8b2bbbb4562428edd03bfced67cb771f2f
andrew <lagovskiy@gmail.com>
2017-05-16 20:29:38 -0400

    Save session data to the file during phpunit testing

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,9 +5,11 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic browser test example.
      *
@@ -15,9 +17,32 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('SocialNet');
+        $user1 = factory(User::class)->create([
+            'username'=>'test1',
+            'email'=>'test1@gustr.com',
+            'password'=>'qwerty'
+        ]);
+
+        $user2 = factory(User::class)->create([
+            'username'=>'test2',
+            'email'=>'test2@gustr.com',
+            'password'=>'qwerty'
+        ]);
+
+
+        $this->browse(function ($first, $second) use ($user1, $user2) {
+            $first->loginAs($user1)
+                ->visit('/home')
+                ->waitForText('Message');
+
+            $second->loginAs($user2)
+                ->visit('/home')
+                ->waitForText('Message')
+                ->type('message', 'Hey Taylor')
+                ->press('Send');
+
+            $first->waitForText('Hey Taylor')
+                ->assertSee('Jeffrey Way');
         });
     }
 }
