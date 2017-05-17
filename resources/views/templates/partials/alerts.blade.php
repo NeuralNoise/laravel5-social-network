f5f8ec6783d0c8c357495c2f6ea8bf0f818c9b00
andrew <lagovskiy@gmail.com>
2017-05-16 20:23:33 -0400

    Add close icon for alerts

--- resources/views/templates/partials/alerts.blade.php
+++ resources/views/templates/partials/alerts.blade.php
@@ -1,5 +1,6 @@
 @if (Session::has('info'))
-	<div class="alert alert-info" role="alert">
+	<div class="alert alert-info alert-dismissable" role="alert">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 		{{ Session::get('info') }}
 	</div>
 @endif
